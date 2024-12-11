09eef0b656c876ea06cb6224863bc595db0cd2c3
Jonny Broughton <jonny.broughton08@gmail.com>
2024-12-11 13:02:34 +0000

    add sorting and product count to project page

--- products/models.py
+++ products/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
-class Category(models.Model):
-    
-    class Meta:
-        verbose_name_plural = "Categories"
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -14,7 +14,6 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
-
 
 
 class Product(models.Model):
