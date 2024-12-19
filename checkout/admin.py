c10218357b24c0e5c0e594867aa8f76916f85dad
Jonny Broughton <jonny.broughton08@gmail.com>
2024-12-19 12:21:56 +0000

    add order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,35 +1,31 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+
 class OrderLineItemAdminInline(admin.TabularInline):
-    """
-    Inline admin for OrderLineItem.
-    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    """
-    Admin configuration for the Order model.
-    """
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date', 
-                       'delivery_cost', 'order_total', 
-                       'grand_total', 'original_bag', 
-                       'stripe_pid',)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name', 
-              'email', 'phone_number', 'country', 
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
+              'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost', 
-              'order_total', 'grand_total')
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
-    list_display = ('order_number', 'date', 'full_name', 
-                    'order_total', 'delivery_cost', 'grand_total')
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-date',)
 
-# Register the Order model with the customized admin
 admin.site.register(Order, OrderAdmin)
